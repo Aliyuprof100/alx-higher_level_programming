816d9101d9e9d9a59d6a2f60b76e80b425005544
aliyuprof100 <aliyuddeen95gmail.com>
2024-09-18 03:47:37 -0700

    9 filed edited

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,9 +1,9 @@
 -- A script that create a second table 
 CREATE TABLE IF NOT EXISTS second_table(id INT, name VARCHAR(256), score INT);
 
--- A script that insert 4 values into the table
+-- A script that insert 4 values into the table created
 
-INSERT INTO second_table(id, name, score) VALUES (1, "john", 10);
-INSERT INTO second_table(id, name, score) VALUES (2, "alex", 3);
-INSERT INTO second_table(id, name, score) VALUES (3, "Bob", 14);
-INSERT INTO second_table(id, name, score) VALUES (4, "George", 8);
+INSERT INTO second_table (id, name, score) VALUES (1, "john", 10);
+INSERT INTO second_table (id, name, score) VALUES (2, "alex", 3);
+INSERT INTO second_table (id, name, score) VALUES (3, "Bob", 14);
+INSERT INTO second_table (id, name, score) VALUES (4, "George", 8);
