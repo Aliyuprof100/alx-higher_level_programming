460d132435e8e124db24f09d18a2d6365440eaa4
aliyuprof100 <aliyuddeen95gmail.com>
2024-09-18 04:41:32 -0700

    9 filed edited

--- 0x0D-SQL_introduction/10-top_score.sql
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -1,2 +1,2 @@
 -- Script that shows the records of the table ODER BY score
-SELECT name, score  FROM second_table ORDER BY score DESC;
+SELECT 'score', 'name'  FROM 'second_table' ORDER BY 'score' DESC;
