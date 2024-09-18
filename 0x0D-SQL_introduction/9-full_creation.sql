43b8f2ccd19400b6ac22d0236460a798d046a591
aliyuprof100 <aliyuddeen95gmail.com>
2024-09-18 03:28:42 -0700

    9-full creation done

---
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -0,0 +1,9 @@
+-- A script that create a second table 
+CREATE TABLE IF NOT EXISTS  second_table(id INT, name VARCHAR(256), score INT);
+
+-- A script that insert 4 values into the table
+
+INSERT INTO second_table(id, name, score) values (1, 'john', 10);
+INSERT INTO second_table(id, name, score) values (2, 'alex', 3);
+INSERT INTO second_table(id, name, score) values (3, 'Bob', 14);
+INSERT INTO second_table(id, name, score) values (4, 'George', 8);
