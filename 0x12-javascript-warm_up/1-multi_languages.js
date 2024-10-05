76d784aab651a4c0a59bacae4bcb3f4be89f94a0
aliyuprof100 <aliyuddeen95gmail.com>
2024-10-04 23:03:25 -0700

    Done two files now in js

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 // A javascript that display a message
 
-console.log("C is fun");
-console.log("Python is cool");
-console.log("Javascript is amazing");
+console.log('C is fun');
+console.log('Python is cool');
+console.log('Javascript is amazing');
