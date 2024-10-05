a7652dc2b166d11617e1626dd7df27a4f065880d
aliyuprof100 <aliyuddeen95gmail.com>
2024-10-05 06:16:49 -0700

    2-arguments.js updated

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -3,8 +3,9 @@
 // When number of Arguments passed
 
 const args = process.argv.slice(2);
-if (args.lengh == 0){
+if (args.lengh === 0){
 	console.log("No argument");
-}else if (args.lengh == 1) {
+}else if (args.lengh === 1) {
 	console.log("Argument found");
-}else 	console.log("Arguments found");
+}else 	
+	console.log("Arguments found");
