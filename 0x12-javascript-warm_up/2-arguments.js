ae20b535d56de79e49106a7b0f99861613db74a6
aliyuprof100 <aliyuddeen95gmail.com>
2024-10-05 06:34:05 -0700

    2-arguments meet semistandard

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,11 +1,12 @@
 #!/usr/bin/node
-// A script that shown diplay a messages 
+// A script that shown diplay a messages.
 // When number of Arguments passed
 
 const args = process.argv.slice(2);
-if (args.lengh === 0){
-	console.log("No argument");
-}else if (args.lengh === 1) {
-	console.log("Argument found");
-}else 	
-	console.log("Arguments found");
+if (args.lengh === 0) {
+  console.log('No argument');
+} else if (args.lengh === 1) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
+}
