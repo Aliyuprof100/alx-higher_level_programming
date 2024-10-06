98f23613894d40a839025828ccb3fd9f033ab75f
aliyuprof100 <aliyuddeen95gmail.com>
2024-10-06 05:42:14 -0700

    9 is done

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -3,12 +3,4 @@ function add (a, b) {
   return a + b;
 }
 
-const firstInteger = Math.floor(Number(process.argv[2])); // Convert the first argument to an integer
-const secondInteger = Math.floor(Number(process.argv[3])); // Convert the second argument to an integer
-
-if (isNaN(firstInteger) || isNaN(secondInteger)) {
-  console.log('Missing parameters'); // Print if either argument is not a number
-} else {
-  const result = add(firstInteger, secondInteger); // Call the add function
-  console.log(result); // Print the result of the addition
-}
+console.log(add(Number(process.argv[2])), add(Number(process.argv[3])));
