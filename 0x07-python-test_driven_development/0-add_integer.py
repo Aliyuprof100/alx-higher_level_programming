f1a95157e1ac5a64556310c24d416cb5b0952d5c
aliyuprof100 <aliyuddeen95@gmail.com>
2024-11-27 13:22:40 -0800

    0-add_integer

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+""" This is the header file """
+
+def add_integer(a, b=98):
+    """
+    Adds two integers.
+    
+    Parameters:
+    a: Must be an integer or float
+    b: Must be an integer or float (default: 98)
+    
+    Returns:
+    The sum of a and b as an integer.
+    """
+    if not isinstance(a, (int, float)):
+        raise TypeError("a must be an integer")
+    if not isinstance(b, (int, float)):
+        raise TypeError("b must be an integer")
+
+    a = int(a)
+    b = int(b)
+
+    return a + b
