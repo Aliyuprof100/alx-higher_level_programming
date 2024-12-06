9e8a5e581bd8cc6e4bff184a769f3e016e8b059e
aliyuprof100 <aliyuddeen95@gmail.com>
2024-12-06 14:39:25 -0800

    5-text indent

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""This is Question 5 of 'indentation' """
+
+
+def text_indentation(text):
+    """
+    Prints a text with 2 new lines after each '.', '?', and ':'.
+    Args:
+        text (str): The input text to format.
+
+    Raises:
+        TypeError: If text is not a string.
+    """
+    if not isinstance(text,str):
+        raise TypeError("text must be a string")
+    result = ""
+    for char in text:
+        result += char
+        if char in ".?:":
+            result += "\n\n"
+
+    # Print the result without leading/trailing spaces on each line
+    print("\n".join([line.strip() for line in result.split("\n")]))
